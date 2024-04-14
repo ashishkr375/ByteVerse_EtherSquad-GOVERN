583f112a3165761474249396a71301b484947997
Ashish Kumar <114145991+ashishkr375@users.noreply.github.com>
2024-04-14 13:55:55 +0530

    Fixed Errors

--- govchain/src/components/NavbarG.js
+++ govchain/src/components/NavbarG.js
@@ -1,7 +1,6 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { useAuth0 } from "@auth0/auth0-react";
 import navlogotran from '../images/whitebgtrans.png'
 const navigation = [
   { name: 'Home', href: '#', current: true },
@@ -15,8 +14,6 @@ function classNames(...classes) {
 }
 
 export default function NavbarG() {
-  
- const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
@@ -70,24 +67,7 @@ export default function NavbarG() {
                   <span className="sr-only">View notifications</span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
-                {/* // to toggle the login and logout button  */}
-          {isAuthenticated ? (
-            <li>
-              <button onClick={() => logout({ returnTo: window.location.origin })} class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-          <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-          Log Out
-          </span>
-          </button>
-            </li>
-          ) : (
-            <li><button onClick={() => loginWithRedirect()} class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
-            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-            Log In
-            </span>
-            </button>
-            
-            </li>
-          )}
+
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
@@ -111,22 +91,34 @@ export default function NavbarG() {
                     leaveTo="transform opacity-0 scale-95"
                   >
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <Menu.Item>
+                      <Menu.Item>
                         {({ active }) => (
-                          <p> {isAuthenticated && (
-            <li>
-              <p> {user.name} </p>
-            </li>
-          )}</p>
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Your Profile
+                          </a>
                         )}
                       </Menu.Item>
                       <Menu.Item>
                         {({ active }) => (
-                          <button onClick={() => logout({ returnTo: window.location.origin })} class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-                          <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                          Log Out
-                          </span>
-                          </button>
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Settings
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Sign out
+                          </a>
                         )}
                       </Menu.Item>
                     </Menu.Items>
