6e5de66c3013e04e4e06e3af8a2339afa43e1ecb
portfolio <diyaag2020@gmail.com>
2024-04-14 06:07:30 +0530

    updated citizen page ui

--- govchain/src/pages/CitizenPage.js
+++ govchain/src/pages/CitizenPage.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import GlassCardC from '../components/GlassCardC'
 import NavbarC from '../components/NavbarC'
+import '../App.css'
 function CitizenPage() {
   return (
     <>
         <NavbarC/>
-        CitizenPage
+        <div className="area">
+        <ul class="circles">
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+		
+        <GlassCardC/>
+        </div>
     </>
   )
 }
