b409fb5f4e62d2e2decd43f5ba791afb0d6f8bb4
Ashish <kumarashish98526@gmail.com>
2024-04-14 08:36:21 +0530

    Updated File Sharing

---
+++ govchain/src/components/File.js
@@ -0,0 +1,78 @@
+import Files from "./artifacts_File/Files.json";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import FileUpload from "./FileUpload";
+import Display from "./Display";
+import Modal from "./Modal";
+import "./File.css";
+
+function File() {
+  const [account, setAccount] = useState("");
+  const [contract, setContract] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const loadProvider = async () => {
+      if (provider) {
+        window.ethereum.on("chainChanged", () => {
+          window.location.reload();
+        });
+
+        window.ethereum.on("accountsChanged", () => {
+          window.location.reload();
+        });
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount(address);
+        let contractAddress = "0x56d2439aDE15fF6B9379b79Be6577545f7FAF7EB";
+
+        const contract = new ethers.Contract(
+          contractAddress,
+          Files.abi,
+          signer
+        );
+        //console.log(contract);
+        setContract(contract);
+        setProvider(provider);
+      } else {
+        console.error("Metamask is not installed");
+      }
+    };
+    provider && loadProvider();
+  }, []);
+  return (
+    <>    
+      {!modalOpen && (
+        <button className="share" onClick={() => setModalOpen(true)}>
+          Share
+        </button>
+      )}
+      {modalOpen && (
+        <Modal setModalOpen={setModalOpen} contract={contract}></Modal>
+      )}
+
+      <div className="App">
+        <h1 style={{ color: "black" }}>Gov File Transfer</h1>
+        <div class="bg"></div>
+        <div class="bg bg2"></div>
+        <div class="bg bg3"></div>
+
+        <p style={{ color: "black" }}>
+          Account : {account ? account : "Not connected"}
+        </p>
+        <FileUpload
+          account={account}
+          provider={provider}
+          contract={contract}
+        ></FileUpload>
+        <Display contract={contract} account={account}></Display>
+      </div>
+    </>
+  );
+}
+
+export default File;
